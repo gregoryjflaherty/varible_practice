fc30e7e1941ef6bd86d5d1603a1f5144f0b2107c
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-19 08:53:49 -0400

    Add floats and integers

--- model-1.rb
+++ model-1.rb
@@ -1,3 +1,12 @@
+# strings
 student_1 = "Greg"
 student_2 = "Saba"
 student_3 = "Austin"
+
+# integers and floats
+num_of_students = 3
+num_of_lesson = 4
+num_of_mod = 0 
+num_of_directions = 17
+current_direction = 9.5
+directions_remaining = 7.5
